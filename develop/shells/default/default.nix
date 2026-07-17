7189d57ce138aec4a6d9a0f55951c40ea812117e
Sobte <i@sobte.me>
2026-07-18 03:42:37 +0800

    migrate: snowfall-lib → purr, pre-commit-hooks → git-hooks
    - Replace snowfall-lib with purr flake framework
    - Rename pre-commit-hooks input to git-hooks
    - Rename checks/pre-commit-check to checks/git-hooks
    - Update shell and check references
    - Rewrite README with purr + git-hooks template style

--- develop/shells/default/default.nix
+++ develop/shells/default/default.nix
@@ -1,23 +1,23 @@
 {
   inputs,
   pkgs,
-  mkShell,
   system,
   ...
 }:
 let
-  jdk = pkgs.graalvmPackages.graalvm-ce; # pkgs.jdk23;
+  jdk = pkgs.graalvmPackages.graalvm-ce;
   maven = pkgs.maven.override { jdk_headless = jdk; };
   gradle = pkgs.gradle.override { java = jdk; };
 in
-mkShell {
+pkgs.mkShell {
   packages = [
     jdk
     maven
     gradle
-    # ant
   ];
 
-  inherit (inputs.self.checks.${system}.pre-commit-check) shellHook;
-  buildInputs = inputs.self.checks.${system}.pre-commit-check.enabledPackages;
+  shellHook = ''
+    ${inputs.self.checks.${system}.git-hooks.shellHook}
+  '';
+  buildInputs = inputs.self.checks.${system}.git-hooks.enabledPackages;
 }
